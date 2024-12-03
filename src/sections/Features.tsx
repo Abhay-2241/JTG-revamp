1908792a1ad2778955ffd6bf8334644ad4bbfe45
abhay das <dasabhay215@gmail.com>
2024-12-03 21:02:56 +0530

    changes 2

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -77,7 +77,7 @@ export const Features = () => {
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-20">
               <div>
                 <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-200 leading-tight">
-                  JTG's Key Expertise
+                  JTGs Key Expertise
                 </h2>
                 <ul className="mt-12 flex flex-col gap-8">
                   {features.map((feature) => (
